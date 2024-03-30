59fec787a2ebce62e5fbbca0efdc5f3c377cc7e1
tsaxking <taylorking206@u.boisestate.edu>
2024-03-30 08:39:34 -0600

    start dashboard

--- client/views/pages/Dashboard.svelte
+++ client/views/pages/Dashboard.svelte
@@ -1,1 +1,20 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { onMount } from "svelte";
+import { Bucket } from "../../models/transactions/bucket";
+
+let buckets: Bucket[] = [];
+
+    onMount(() => {
+        Bucket.all().then((b) => {
+            if (b.isErr()) return console.error(b.error);
+            buckets = b.value;
+        });
+
+
+
+        return () => {
+            // cleanup
+            buckets = [];
+        }
+    });
+</script>
