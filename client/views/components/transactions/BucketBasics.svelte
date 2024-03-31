172c60d3b4f0a169c4cc2b399d9ec5e6d98f66ce
tsaxking <taylorking206@u.boisestate.edu>
2024-03-31 14:00:00 +0100

    start transaction table

---
+++ client/views/components/transactions/BucketBasics.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { onMount } from "svelte";
+import { Bucket } from "../../../models/transactions/bucket";
+
+    export let bucket: Bucket;
+    export let from: number;
+    export let to: number;
+
+    onMount(() => {
+        Promise.all([
+            bucket.getBalance(from, to)
+        ]).then(([
+            balances
+        ]) => {});
+        
+        return () => {}
+    });
+</script>
--- client/views/pages/Dashboard.svelte
+++ client/views/pages/Dashboard.svelte
@@ -1,20 +1,35 @@
 <script lang="ts">
-import { onMount } from "svelte";
+import SelectBucket from "../components/transactions/SelectBucket.svelte";
 import { Bucket } from "../../models/transactions/bucket";
+import { onMount } from "svelte";
+import DashboardCard from "../components/main/DashboardCard.svelte";
+import BucketBasics from "../components/transactions/BucketBasics.svelte";
+
 
 let buckets: Bucket[] = [];
+let search: string = '';
 
-    onMount(() => {
-        Bucket.all().then((b) => {
-            if (b.isErr()) return console.error(b.error);
-            buckets = b.value;
-        });
-
-
-
-        return () => {
-            // cleanup
-            buckets = [];
-        }
-    });
+let from: number = Date.now();
+let to: number = Date.now();
 </script>
+
+<div class="container-fluid">
+    <div class="row mb-3">
+        <SelectBucket bind:active={buckets} />
+
+        <div class="col-12">
+            <div class="input-group">
+                <input type="date" class="form-control" bind:value={from} />
+                <input type="date" class="form-control" bind:value={to} />
+                <input type="text" class="form-control" bind:value={search} />
+            </div>
+        </div>
+    </div>
+    <div class="row mb-3">
+        {#each buckets as bucket}
+            <DashboardCard title="Summary: {bucket.name}">
+                <BucketBasics {bucket} {from} {to} />
+            </DashboardCard>
+        {/each}
+    </div>
+</div>
