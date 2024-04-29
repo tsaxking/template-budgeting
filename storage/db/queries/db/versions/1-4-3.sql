be871eb6567a9192afb0f49b77bea4de6a16e194
Taylor King <taylorking206@u.boisestate.edu>
2024-04-28 20:42:06 -0600

    start goals and budgeting

---
+++ storage/db/queries/db/versions/1-4-3.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS Goals (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    amount SMALLMONEY NOT NULL,
+    arhived BOOLEAN NOT NULL DEFAULT FALSE,
+    created BIGINT NOT NULL,
+    description TEXT NOT NULL,
+    budgetId TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Budgets (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    amount SMALLMONEY NOT NULL,
+    arhived BOOLEAN NOT NULL DEFAULT FALSE,
+    created BIGINT NOT NULL,
+    description TEXT NOT NULL
+);
